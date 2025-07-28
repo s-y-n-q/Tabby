e003657b2280e01f9d0237cbbeba73682f4e18da
synq <synqie>
2025-07-27 22:11:24 -0700

    test

---
+++ TabbyClient/Patches/Patches.cs
@@ -0,0 +1,19 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabbyClient.Patches
+{
+    [HarmonyPatch(typeof(PlayFabAuthenticator), "Awake")]
+    public class FallDamagePatch
+    {
+        private static bool Prefix()
+        {
+            
+        }
+    }
+}
---
+++ TabbyClient/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabbyClient
+{
+    [BepInPlugin("synq.gorilla.tabby", "Tabby Client", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony harmony = new Harmony("synq.gorilla.tabby");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo("Initialized Tabby Client!");
+        }
+    }
+
+    // exp
+    /*
+    [HarmonyPatch(typeof(CharacterMovement), "CheckFallDamage")]
+    public class FallDamagePatch
+    {
+        private static bool Prefix()
+        {
+            UI.NewDebugNotif("Patched CharacterMovement.checkfalldamage");
+            return !Self.NFD;
+        }
+    }*/
+}
